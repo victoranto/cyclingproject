391c192674df2ffc976d0998c2e36178d978f6cb
Victor Gonzalez <victoranto2@gmail.com>
2022-10-15 07:30:47 +0200

    Fix redirect after Strava login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,11 @@
 import React from 'react';
-//import { useNavigate } from 'react-router-dom'
-const { REACT_APP_CLIENT_ID } = process.env
-const redirectUrl = "http://localhost:3000/redirect"
+
+const { REACT_APP_CLIENT_ID, REDIRECT_URL } = process.env
 
 export default function Login(){
-  //const navigate = useNavigate()
   
-  // const handleButton = () => {
-  //   navigate('/' )
-  // }
   const handleLogin = () => {
-    //window.location = `http://www.strava.com/oauth/authorize?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${redirectUrl}/exchange_token&approval_prompt=force&scope=activity:read_all`;
-    window.location = `http://www.strava.com/oauth/authorize?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${redirectUrl}/?approval_prompt=force&scope=activity:read_all`;
+    window.location = `http://www.strava.com/oauth/authorize?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URL}/?approval_prompt=force&scope=activity:read_all`;
   };
 
 
